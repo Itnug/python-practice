7a582ad9328ff47a42bc2e8bf5834710c1b388f6
itnug <srnvsiit@gmail.com>
2020-12-18 02:49:33 +0530

    clean up p70

--- project_euler/p70.py
+++ project_euler/p70.py
@@ -12,7 +12,6 @@
 
 # φ(87109)=79180 ~~~~ n/φ(n) = 1.1001389239707
 
-from math import gcd
 from timeit import timeit
 from itertools import count
 import numpy as np
@@ -33,31 +32,13 @@ def primes():
             mem[i + i + i] = [i + i]
 
 
-def phi_1(n):
-    if n == 1:
-        return 1
-    c = 1
-    for i in range(2, n):
-        if gcd(i, n) == 1:
-            c += 1
-    return c
-
-
-def phi_2(n):
-    return (np.gcd(np.arange(2, n), n) == 1).sum() + 1
-
-
 def is_perm(a, b):
     return sorted(str(a)) == sorted(str(b))
 
 
-def skip(n, p_array):
-    return np.any(n % p_array == 0)
-
-
 def prime_factorize(n, primes_list):
     prime_factors = []
-    i = 1
+    i = 0
     while n != 1:
         c = 0
         p = primes_list[i]
@@ -67,7 +48,7 @@ def prime_factorize(n, primes_list):
             n //= p
 
         if c > 0:
-            prime_factors.append((p,c))
+            prime_factors.append((p, c))
 
     return prime_factors
 
@@ -80,40 +61,16 @@ def phi(n, primes_list):
     return product
 
 
-def main():
-    start = timeit.default_timer()
-
-    p_list = []
-    for i in primes():
-        if i / (i-1) < 1.0012471656911804:
-            break
-        p_list.append(i)
-    p_array = np.array(p_list)
-    min_m = 9999
-    min_n = 1
-    for n in tqdm(range(3298093, 10000000, 2)):
-        # 2868469
-        if skip(n, p_array):
-            continue
-        phi_n = phi_2(n)
-        if is_perm(n, phi_n):
-            m = n/phi_n
-            split = timeit.default_timer()
-            if m < min_m:
-                min_n = n
-                min_m = m
-                print(n, phi_n, m, split-start, '\t!')
-            else:
-                print(n, phi_n, m, split-start)
-
-    print(min_n)
-
-def main2(max_n):
+def main(max_n):
     primes_gen = primes()
-    p = next(primes_gen)
-    primes_list = [p]
-    p = next(primes_gen)
+    skip_list = []
+    primes_list = []
 
+    p = next(primes_gen)
+    while p < 1000:
+        skip_list.append(p)
+        p = next(primes_gen)
+    skip_list = np.array(skip_list)
     min_n = 3
     min_m = 2
     for n in tqdm(range(3, max_n, 2)):
@@ -121,7 +78,7 @@ def main2(max_n):
             primes_list.append(p)
             p = next(primes_gen)
             continue
-        if any(n % x == 0 for x in [3, 5, 7, 11]):
+        if np.any(n % skip_list == 0):
             continue
         phi_n = phi(n, primes_list)
         if is_perm(n, phi_n):
@@ -132,8 +89,9 @@ def main2(max_n):
                 print(n, phi_n, m)
     return min_n
 
+
 def T1():
-    print("answer =", main2(10000000))
+    print("answer =", main(10000000))
 
 
 with cProfile.Profile() as pr:
@@ -141,4 +99,3 @@ with cProfile.Profile() as pr:
     print(t1)
 
 pr.print_stats(sort='cumtime')
-# print(phi(87109, primes()))
